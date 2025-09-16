a5a7d81781bfb6fe3fe61807bf45f2a1a66b814d
dhan04shree <dhanshreemagar0@gmail.com>
2025-09-16 18:20:04 +0530

    constructor code

--- constructor.cpp
+++ constructor.cpp
@@ -16,20 +16,33 @@ class Complex{
     private:
     int a,b;
     public:
-    Complex(int x,int y){
+    Complex(int x,int y){ //parameterized constructor
         cout<<"Hello i am cons"<<endl;
         a = x;
         b = y;
     }
-    Complex(int x){ //constructor overloading
+    Complex(int x){ //parameterized //constructor overloading
         a = x;
         cout<<"a = "<<a<<endl;
+    }
+    Complex(){ //default constructor
+    }
+    Complex(Complex &c){ //recursion problem //thats why we take here reference variable
+        a = c.a;
+        b = c.b;
+        //c1 ka data c4 me ho gya hai copy
     }
 
 };
 int main(){
     // Complex c1;
     // Complex c2; //jitne bar object bnavonge utne bar constructor chlega aur "Hello i am cons" print hoga
-    // Complex c1(3,5),c2(5);
-    Complex c1 = Complex(3,8),c2=5;
+    Complex c1(3,5),c2(5);
+    // Complex c1 = Complex(3,8),c2=5; //another method of calling
+    //object bnte hi compiler default constructor bnata hai..but agar ham c1(3,5) ko call kre aur koi Complex(int x,int y) nhi bnaye to error aata hai
+    //aur agar hm ek bhi argu. lene wala Complex(int x,int y) bnaye to compiler default constructor nhi bnata.
+
+    //compiler 2 types ke constructor bna skta hai by default 1) Default 2)Copy ..agar hmne construcotr bna diya hai to vo default nhi bnata but copy bnata hai
+    //hmne copy bhi cons. bhi bnaya to compiler nhi bnayenga
+    Complex c4(c1); //copy constructor bcz object bnte vkt argument me same class ka object as a argument jata hai.
 }
