fc9fcd78174ce234ac9c2f5d7cccac7f7872a275
dhan04shree <dhanshreemagar0@gmail.com>
2025-09-18 22:06:21 +0530

    inheritance

---
+++ inheritance.cpp
@@ -0,0 +1,16 @@
+//It is a process of inheriting properties and behaviours of existing class into a new class
+//existing class = old class = parent class = base class
+// new class = child class = derived class
+#include<iostream>
+using namespace std;
+
+class Car{
+    
+};
+
+class SportsCar : public Car{
+
+};
+int main(){
+
+}
